c6a9791b04c42adfc3500d65ad7c156ea988e06a
Shay Palachy <shaypal5@gmail.com>
2019-07-13 14:48:57 +0300

    added py3.7 trove classifier

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Intended Audience :: Developers',
