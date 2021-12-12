48ce16ff07710cde956ff3a7071d83cbbd0f9977
Dimid Duchovny <dimidd@localize.city>
2021-12-12 20:52:51 +0200

    core: add SeriesFtClassifier
    * add a wrapper for pandas series.
    * update README
    * add a simple unittest for fit and predict
    
    Signed-off-by: Dimid Duchovny <dimidd@localize.city>

--- skift/__init__.py
+++ skift/__init__.py
@@ -4,6 +4,7 @@ from .core import FirstColFtClassifier  # noqa: F401
 from .core import IdxBasedFtClassifier  # noqa: F401
 from .core import FirstObjFtClassifier  # noqa: F401
 from .core import ColLblBasedFtClassifier  # noqa: F401
+from .core import SeriesFtClassifier  # noqa: F401
 
 from ._version import get_versions
 __version__ = get_versions()['version']
